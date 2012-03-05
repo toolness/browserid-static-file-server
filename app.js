18914ed0070adde06a84820c0acfdbd19968546b
Atul Varma <varmaa@gmail.com>
2012-03-04 21:12:26 -0500

    Origination.

---
+++ app.js
@@ -0,0 +1,62 @@
+const FORBIDDEN_HTML = 'Permission denied! Try <a href="/login/">logging in</a>.';
+const COOKIE_SECRET = "lolwut";
+
+var express = require('express'),
+    path = require('path'),
+    browserid = require('./browserid.js'),
+    fs = require('fs');
+
+var app = express.createServer();
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: COOKIE_SECRET}));
+
+app.post('/login/authenticate', function(req, res) {
+  if (!req.body || !req.body['assertion'])
+    return res.send({message: 'assertion expected'}, 400);
+  var uri = 'https://browserid.org:443/verify';
+  var audience = 'localhost';
+  var assertion = req.body['assertion'];
+  browserid.verify(uri, assertion, audience, function(err, verifierResponse) {
+    if (err) {
+      return res.send({message: 'verifier returned error'}, 400);
+    }
+    req.session.email = verifierResponse.email;
+    return res.send({email: verifierResponse.email});
+  });
+});
+
+app.use(function(req, res, next) {
+  var p = path.normalize(req.path);
+  if (!req.session.email) {
+    res.setHeader('Location', '/login/?redirect=' + encodeURIComponent(p));
+    res.statusCode = 302;
+    return res.end();
+  }
+  return next();
+});
+
+app.wwwDir = path.join(__dirname, 'www');
+app.wwwMiddleware = express.static(app.wwwDir);
+app.use(function(req, res, next) {
+  var p = path.normalize(req.path);
+  var filename = path.join(app.wwwDir, p);
+  var dirname = path.dirname(filename);
+  var browseridaccess = path.join(dirname, '.browseridaccess');
+  
+  fs.readFile(browseridaccess, 'utf-8', function(err, emails) {
+    if (err) {
+      if (err.code == 'ENOENT')
+        return res.send(FORBIDDEN_HTML, 403);
+      throw err;
+    }
+    emails = emails.split('\n');
+    if (emails.indexOf(req.session.email) == -1)
+      return res.send(FORBIDDEN_HTML, 403);
+    return app.wwwMiddleware(req, res, next);
+  });
+});
+
+module.exports = app;
---
+++ test.js
@@ -0,0 +1,77 @@
+const PORT = 3001;
+
+var app = require('./app.js'),
+    APIeasy = require('api-easy'),
+    assert = require('assert'),
+    browserid = require('./browserid.js'),
+    express = require('express'),
+    path = require('path');
+
+app.listen(PORT);
+
+browserid.verify = function(url, assertion, audience, cb) {
+  if (assertion != "assertion for example@example.com")
+    setTimeout(function() { cb("fail"); }, 10);
+  else
+    setTimeout(function() { cb(null, {email: "example@example.com"}); }, 10);
+};
+
+var suite = APIeasy.describe("server");
+
+suite.expectRedirectTo = function(path) {
+  return this.expect(302)
+    .expect('redirects to ' + path, function(err, res) {
+      assert.equal(res.headers['location'], path);
+    });
+};
+
+suite._requestFormData = function(method, data) {
+  data = data || {};
+  if (!('_csrf' in data))
+    data._csrf = module.exports.FAKE_UID;
+  this.setHeader('Content-Type', 'application/x-www-form-urlencoded')
+    [method](data);
+  return this;
+};
+
+suite.postFormData = function(data) {
+  return this._requestFormData('post', data);
+};
+
+app.wwwDir = path.join(__dirname, 'www-test');
+app.wwwMiddleware = express.static(app.wwwDir);
+
+suite
+  .use('localhost', PORT).followRedirect(false)
+  .discuss('when logged out')
+    .path('/')
+      .get()
+      .expectRedirectTo("/login/?redirect=%2F")
+      .unpath()
+    .path('/login/authenticate')
+      .post()
+      .expect(400, {message: 'assertion expected'})
+      .postFormData({assertion: "lol"})
+      .expect(400, {message: 'verifier returned error'})
+      .next()
+      .postFormData({assertion: "assertion for example@example.com"})
+      .expect(200, {email: "example@example.com"})
+      .next()
+    .unpath().undiscuss()
+  .discuss('when logged in')
+    .path('/example-only/foo.html')
+      .get()
+      .expect(200)
+      .expect("body is 'this is foo'", function(err, req) {
+        assert.equal(req.body, "this is foo");
+      })
+      .unpath()
+    .path('/nonexistent-directory/bar.html')
+      .get()
+      .expect(403)
+      .unpath()
+    .path('/another-example-only/bar.html')
+      .get()
+      .expect(403);
+
+suite.export(module);
